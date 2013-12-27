08aeade4c02696c72fa1fe141b9246ba082740e7
Shahbaz Khan <shahbaz.khan1003@gmail.com>
2013-12-28 02:20:10 +0530

    Create DecodingBase64Message.java
    For decoding base63 encoded incoming files

---
+++ digitalSignature/src/main/java/com/shahbaz/digital/digitalSignature/DecodingBase64Message.java
@@ -0,0 +1,30 @@
+package com.shahbaz.digital.digitalSignature;
+
+import org.jasypt.contrib.org.apache.commons.codec_1_3.binary.Base64;
+
+/**
+ * 
+ * @author shabby
+ *
+ */
+public class DecodingBase64Message {
+
+	public static void main(String[] args) {
+		
+		String message="SGVsbG8gd29ybGQgISBJIHdhbnQgdG8gYmUgc2lnbmVk";
+		
+		System.out.println(" Messag was  -- >>  "+new DecodingBase64Message().decodeBase64(message));
+	}
+	
+	/**
+	 * Decode the Base64 Message 
+	 * 
+	 * @param Base64 message
+	 * @return Decoded Message
+	 */
+	public String decodeBase64(String message){
+		
+		return new String(Base64.decodeBase64(message.getBytes()));
+	}
+	
+}
